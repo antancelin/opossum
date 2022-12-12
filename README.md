7ece2c4641296d05338135f092fcc1a946ddd375
antancelin <antoineancelin@icloud.com>
2022-12-12 15:03:27 +0100

    Dernière version brief opossum

---
+++ README.md
@@ -0,0 +1,7 @@
+# Déterminer l'âge d'un opossum
+
+---
+
+Il s'agit ici de pouvoir créer un modèle de régression linéaire permettant de déterminer l'âge d'un opossum en fonction de ses caractéristiques.
+
+Le meilleur score a été obtenu via KNN Regressor avec un RMSE de 1,79 et un R² de 0,29. Le score est assez faible, il aurait fallu plus de données afin d'améliorer l'entraînement.
--- opossum.py
+++ opossum.py
@@ -7,19 +7,18 @@ scaler = joblib.load('model_opossum.joblib')['scaler']
 
 def age_determine(model, hdlngth, skullw, totlngth, eye, chest, belly):
     
-    age = {0:'0', 1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9', 10:'10'}
     x = np.array([hdlngth, skullw, totlngth, eye, chest, belly]).reshape(1,6)
     x_scaled = scaler.transform(x)
-    return age[model.predict(x_scaled)[0]]
+    return model.predict(x_scaled)[0]
 
-st.title("Opossum")
-hdlngth = st.slider('Taille de la tête (en mm)', 70.0, 105.0, format="%f")
-skullw = st.slider('Largeur du crâne (en cm)', 50.0, 80.0, format="%f")
-totlngth = st.slider('Longueur totale (en cm)', 65.0, 105.0, format="%f")
-eye = st.slider('Taille des yeux (en mm)', 10.0, 20.0, format="%f")
-chest = st.slider('Tour de poitrine (en cm)', 15.0, 40.0, format="%f")
-belly = st.slider('Tour de ventre (en cm)', 15.0, 50.0, format="%f")
+st.title("Âge d'un opossum")
+head = st.slider('Taille de la tête (en mm)', 70.0, 105.0, format="%f")
+skull = st.slider('Largeur du crâne (en cm)', 50.0, 80.0, format="%f")
+totalL = st.slider('Longueur totale (en cm)', 65.0, 105.0, format="%f")
+eye_dist = st.slider('Taille des yeux (en mm)', 10.0, 20.0, format="%f")
+chest_size = st.slider('Tour de poitrine (en cm)', 15.0, 40.0, format="%f")
+belly_size = st.slider('Tour de ventre (en cm)', 15.0, 50.0, format="%f")
 
-prediction = age_determine(model, hdlngth, skullw, totlngth, eye, chest, belly)
+prediction = age_determine(model, head, skull, totalL, eye_dist, chest_size, belly_size)
 
-st.write("L'opossum avec ces caractéristiques a :", prediction,"ans")
+st.write("L'opossum avec ces caractéristiques a :", round(prediction, 1), "ans")
